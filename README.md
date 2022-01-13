27951a6e410f2c3d244a68b3f35c6039cc67abb6
cayocesaras <cayo_cesar_as@hotmail.com>
2022-01-12 21:56:19 -0400

    12-01-2022

---
+++ README.md
@@ -0,0 +1,15 @@
+## Aplicação para Previsão de dados com Faceboock Prophet
+
+Você gostaria de saber o valor de um determinado item em uma data futura?
+Se você tem uma planilha ou arquivo de texto com dados históricos, use essa
+aplicação para gerar informações para o mesmo dado em períodos futuros de forma
+simples e rápida. É só seguir o passo a passo:
+
+- Primeiro: No menu, configure os parâmetros para o seu arquivo e a cor dos gráficos.
+- Segundo: Selecione o arquivo com os dados históricos em formato de tabela.
+- Terceiro: Escolha a coluna que contém as datas.
+- Quarto: Escolha a coluna que contém a informação que será prevista.
+- Quinto: Escolha a quantidade de anos (365 dias) que deseja fazer a previsão.
+
+obs.: se houverem muitos dados e quiser usar um campo para realizar um filtro ex.: cliente,
+selecione a coluna para realiazar a filtragem após o Quinto passo, e depois escolha o filtro.
--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import pandas as pd
 from fbprophet.plot import plot_yearly
 from PIL import Image
 import streamlit.components.v1 as components
+import requests
 
 # Definindo o layout da aplicacao
 st.set_page_config(page_title='FastPredict',layout="wide")
@@ -38,100 +39,141 @@ separador   = st.sidebar.selectbox("Qual carecter é usado para separar as colun
 cabecalho   = st.sidebar.number_input("Em qual linha está o seu cabeçalho?", min_value = 1, format = '%d')
 color_graphs = st.sidebar.color_picker("Escolha uma cor para os gráficos:")
 
-# Escolha o arquivo de dados
-upload_file = st.file_uploader("Escolha o arquivo para a análise", type='csv')
+# Seleciona a coluna de data
+arquivos = ["","ARQUIVO","API JSON"]
+tipo_carga = st.selectbox("Selecione a forma de carregamento dos dados:",arquivos)
 
-if upload_file is not None:
-    # Carregando os dados
-    dados = pd.read_csv(upload_file, header=(cabecalho-1), sep=separador, dtype=str)
+# Verifica se o campo está preenchido
+if tipo_carga != "":
+    if tipo_carga == "ARQUIVO":
+        # Escolha o arquivo de dados
+        fonte_dados = st.file_uploader("Escolha o arquivo para a análise", type=['csv','txt','xls','xlsx'])
+        # Verifica se foi selecionado um arquivo
+        if fonte_dados is not None:
+            # Verifica se é um arquivo de TEXTO ou Planilha do Excel
+            if ("excel" in fonte_dados.type) or ("officedocument" in fonte_dados.type):
+                dados = pd.read_excel(fonte_dados, header=(cabecalho-1))
+            else:
+                dados = pd.read_csv(fonte_dados, header=(cabecalho-1), sep=separador, dtype=str)
+    else:
+        # Usar um serviço de API
+        fonte_dados = st.text_input("Digite o endereço URL da API")
+        # Verifica se um endereco foi inserido
+        if fonte_dados != "":
+            dados = pd.read_json(fonte_dados)
+    
+    # Verifica se exite uma fonte de dados selecionada
+    if 'dados' in globals():
+        # Criando lista em branco para colunas
+        colunas = [""]
 
-    # Criando lista em branco para colunas
-    colunas = [""]
+        # Adicionando o nome dos campos do dataframe
+        colunas.extend(dados.columns.to_list())
 
-    # Adicionando o nome dos campos do dataframe
-    colunas.extend(dados.columns.to_list())
+        # Seleciona a coluna de data
+        coluna_data = st.selectbox("Selecione a Coluna de Data:",colunas)
 
-    # Seleciona a coluna de data
-    coluna_data = st.selectbox("Selecione a Coluna de Data:",colunas)
+        # Seleciona a coluna para analise
+        coluna_analise = st.selectbox("Selecione a Coluna para Análise:",colunas)
 
-    # Seleciona a coluna para analise
-    coluna_analise = st.selectbox("Selecione a Coluna para Análise:",colunas)
+        # Seleciona a coluna para filtragem
+        coluna_filtro = st.selectbox("Selecione a Coluna para Filtro:",colunas)
 
-    # Seleciona a coluna para filtragem
-    coluna_filtro = st.selectbox("Selecione a Coluna para Filtro:",colunas)
+        if coluna_filtro != "":
+            # Seleciona item para filtro dos dados
+            filtro = [""]
+            filtro.extend(dados[coluna_filtro].unique().tolist())
+            filtro = st.selectbox("Selecione Item para Filtro:",filtro)
 
-    if coluna_filtro != "":
-        # Seleciona item para filtro dos dados
-        filtro = [""]
-        filtro.extend(dados[coluna_filtro].unique().tolist())
-        filtro = st.selectbox("Selecione Item para Filtro:",filtro)
+        # Verificando se campos data e analise estao preenchidos
+        if coluna_data != "" and coluna_analise != "":
+            # Removendo linhas com valores nulos
+            dados2 = dados.dropna(subset=[coluna_data,coluna_analise])
 
-    # Verificando se campos data e analise estao preenchidos
-    if coluna_data != "" and coluna_analise != "":
-        # Removendo linhas com valores nulos
-        dados2 = dados.dropna(subset=[coluna_data,coluna_analise])
+            # Verifica se ha filtros a serem aplicados
+            if coluna_filtro != "" and filtro != "":
+                # Aplica o filtro aos dados
+                dados2 = dados2[dados2[coluna_filtro] == filtro]
 
-        # Verifica se ha filtros a serem aplicados
-        if coluna_filtro != "" and filtro != "":
-            # Aplica o filtro aos dados
-            dados2 = dados2[dados2[coluna_filtro] == filtro]
+            # Conversao de dados
+            dados2[coluna_data] = pd.to_datetime(dados2[coluna_data])
+            dados2[coluna_analise] = dados2[coluna_analise].replace(',','.', regex=True)
+            dados2[coluna_analise] = dados2[coluna_analise].astype(float)
 
-        # Conversao de dados
-        dados2[coluna_data] = pd.to_datetime(dados2[coluna_data])
-        dados2[coluna_analise] = dados2[coluna_analise].replace(',','.', regex=True)
-        dados2[coluna_analise] = dados2[coluna_analise].astype(float)
+            # Criando coluna de dia da semana
+            dados2["DiadaSemana"] = dados2[coluna_data].dt.day_name()
 
-        # Criando coluna de dia da semana
-        dados2["DiadaSemana"] = dados2[coluna_data].dt.day_name()
+            # Visualizando dados brutos
+            st.subheader("Visualização dos Dados Brutos")
+            st.write(dados2)
 
-        # Visualizando dados brutos
-        st.subheader("Visualização dos Dados Brutos")
-        st.write(dados2)
+            # Dividindo a aplicação em colunas
+            col1, col2 = st.columns(2)
 
-        # Dividindo a aplicação em colunas
-        col1, col2 = st.columns(2)
+            with col1:
+                # Criando o grafico dos dados brutos
+                st.subheader("Quantificação dos Dados")
+                df = px.data.tips()
+                fig = px.histogram(dados2, x=coluna_data, y=coluna_analise, color_discrete_sequence=[color_graphs])
+                fig.update_layout(bargap=0.2)
+                st.plotly_chart(fig,use_container_width=True)
 
-        with col1:
-            # Criando o grafico dos dados brutos
-            st.subheader("Quantificação dos Dados")
-            df = px.data.tips()
-            fig = px.histogram(dados2, x=coluna_data, y=coluna_analise, color_discrete_sequence=[color_graphs])
-            fig.update_layout(bargap=0.2)
-            st.plotly_chart(fig,use_container_width=True)
+            with col2:
+                # Criando grafico de linhas
+                st.subheader("Distribuição dos Dados")
+                df = px.data.tips()
+                fig = px.scatter(dados2, x=coluna_data, y=coluna_analise, color_discrete_sequence=[color_graphs])
+                fig.update_layout(bargap=0.2)
+                st.plotly_chart(fig,use_container_width=True)
 
-        with col2:
-            # Criando grafico de linhas
-            st.subheader("Distribuição dos Dados")
-            df = px.data.tips()
-            fig = px.scatter(dados2, x=coluna_data, y=coluna_analise, color_discrete_sequence=[color_graphs])
-            fig.update_layout(bargap=0.2)
-            st.plotly_chart(fig,use_container_width=True)
+            # Define o horizonte de previsao
+            num_anos = st.slider("Escolha o horizonte de Previsão (em anos):",0,4)
 
-        # Define o horizonte de previsao
-        num_anos = st.slider("Escolha o horizonte de Previsão (em anos):",0,4)
+            if num_anos != 0:
 
-        if num_anos != 0:
+                # Preparando os dados para as previsoes com pacote Prophet
+                df_treino = dados2[[coluna_data,coluna_analise]]
+                df_treino = df_treino.rename(columns = {coluna_data: "ds", coluna_analise: "y"})
 
-            # Preparando os dados para as previsoes com pacote Prophet
-            df_treino = dados2[[coluna_data,coluna_analise]]
-            df_treino = df_treino.rename(columns = {coluna_data: "ds", coluna_analise: "y"})
+                # Criando o Modelo
+                modelo = Prophet(yearly_seasonality=20)
 
-            # Criando o Modelo
-            modelo = Prophet(yearly_seasonality=20)
+                # Treinando o modelo
+                modelo.fit(df_treino)
 
-            # Treinando o modelo
-            modelo.fit(df_treino)
+                # Calcula o periodo de dias
+                periodo = num_anos * 365
 
-            # Calcula o periodo de dias
-            periodo = num_anos * 365
+                # Prepara as datas futuras para as previsoes
+                futuro = modelo.make_future_dataframe(periods = periodo)
 
-            # Prepara as datas futuras para as previsoes
-            futuro = modelo.make_future_dataframe(periods = periodo)
+                # Faz as previsoes
+                forecast = modelo.predict(futuro)
 
-            # Faz as previsoes
-            forecast = modelo.predict(futuro)
+                # Gerando dataframe com os dados previstos
+                ultima_data_dados = dados2[coluna_data].iloc[-1]
+                apenas_previsoes = forecast[(forecast["ds"]>ultima_data_dados)]
 
-            # Criando o grafico das previsoes
-            st.subheader("Gráfico de Dados Preditivos")
-            grafico2 = plot_plotly(modelo, forecast)
-            st.plotly_chart(grafico2,use_container_width=True)
+                # Visualizando dados brutos
+                st.subheader("Visualização dos Dados Previstos")
+                st.write(apenas_previsoes)
+
+                # Criando o grafico das previsoes
+                st.subheader("Gráfico de Dados Preditivos")
+                grafico2 = plot_plotly(modelo, forecast)
+                st.plotly_chart(grafico2,use_container_width=True)
+
+                # Fazer download dos dados previstos
+                @st.cache
+                def convert_df(df):
+                    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+                    return df.to_csv().encode('utf-8')
+
+                csv = convert_df(forecast)
+
+                st.download_button(
+                    label="Faça o download da previsão (CSV)",
+                    data=csv,
+                    file_name='previsao.csv',
+                    mime='text/csv',
+                )
